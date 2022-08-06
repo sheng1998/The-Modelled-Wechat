bac93a9c98ef10f0c4a4f36595eebd892843b2de
z <18475140601@163.com>
2022-08-06 22:13:17 +0800

    feat: 路由器初始化后再挂载app(方便setup阶段获取route.query)

--- src/main.ts
+++ src/main.ts
@@ -5,4 +5,10 @@ import App from './App.vue';
 import '$style/normalize.css';
 import '$style/global.scss';
 
-createApp(App).use(router).mount('#app');
+const app = createApp(App);
+app.use(router);
+
+// 路由器初始化后再挂载app（不然setup里可能获取不到route.query）
+router.isReady().then(() => {
+  app.mount('#app');
+});
