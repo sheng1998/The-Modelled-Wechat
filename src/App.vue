f0c40b765fde3b7f553b33e4a6b9059a9094d5a7
z <18475140601@163.com>
2022-08-12 22:04:51 +0800

    feat: 校验登录状态

--- src/App.vue
+++ src/App.vue
@@ -2,6 +2,20 @@
   <router-view></router-view>
 </template>
 
+<script setup lang="ts">
+import { useRoute, useRouter } from 'vue-router';
+import request from './server';
+
+const route = useRoute();
+const router = useRouter();
+
+// 校验登录状态
+request.get('/login/check').catch(() => {
+  if (route.name === 'Login') return;
+  router.replace({ name: 'Login' });
+});
+</script>
+
 <style lang="scss">
 #app {
   width: 100%;
