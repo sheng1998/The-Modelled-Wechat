96bdf02607dd6ca11666dc7d7e3c185ca8f6981d
z <18475140601@163.com>
2022-08-21 19:01:13 +0800

    feat: 检查登录状态重定向登录页

--- src/App.vue
+++ src/App.vue
@@ -9,11 +9,15 @@ import request from './server';
 const route = useRoute();
 const router = useRouter();
 
-// 校验登录状态
-request.get('/login/check').catch(() => {
-  if (route.name === 'Login') return;
-  router.replace({ name: 'Login' });
-});
+// 非登录页面要检查登录状态（重定向登录页）
+if (route.name !== 'Login') {
+  // 校验登录状态
+  request.get('/user/check').catch((error) => {
+    if (error?.code === 1) {
+      router.replace({ name: 'Login' });
+    }
+  });
+}
 </script>
 
 <style lang="scss">
