3a1d7d2df229635ae9001582f4d588819c728e56
z <18475140601@163.com>
2022-08-27 00:37:26 +0800

    feat: 登录后才允许socket连接

--- src/utils/socket.ts
+++ src/utils/socket.ts
@@ -1,27 +1,40 @@
+/**
+ * socket连接成功
+ *    socket.on('connect', () => void)
+ * socket断开连接
+ *    socket.on('disconnect', () => void)
+ * 用户之间的私聊
+ *    socket.emit('message', { uid, message, type })
+ * 发送群消息
+ *    socket.emit('message-group', { gid, message, type })
+ * 接收到用户的消息
+ *    socket.on('message', { uid, message, type })
+ * 接收到群消息
+ *    socket.on('message-group', { gid, message, type })
+ * 接收到公告消息
+ *    socket.on('notice', { nid, message, type })
+ */
+
 import { io } from 'socket.io-client';
 
-const url = import.meta.env.MODE === 'development' ? 'ws://127.0.0.1:3002' : 'ws://119.91.74.150:3002:3002';
-const socket = io(url, { transports: ['websocket'] });
+export default (uid: string) => {
+  // socket 连接的url
+  const url = import.meta.env.MODE === 'development' ? 'ws://127.0.0.1:3002' : 'ws://119.91.74.150:3002:3002';
+  // socket 连接
+  const socket = io(url, {
+    transports: ['websocket'],
+    auth: { uid },
+  });
 
-socket.on('connect', () => {
-  console.log('socket连接成功!');
-});
+  socket.on('connect', () => {
+    console.log('socket连接成功!');
+  });
 
-socket.on('disconnect', () => {
-  console.log('socket断开连接!');
-  // console.log('socket断开连接, 将自动尝试重连!');
-  // socket = io(url, { transports: ['websocket'] });
-});
+  socket.on('disconnect', () => {
+    console.log('socket断开连接!');
+    // console.log('socket断开连接, 将自动尝试重连!');
+    // socket = io(url, { transports: ['websocket'] });
+  });
 
-// 用户之间的私聊
-// socket.emit('message', { uid, message, type })
-// 发送群消息
-// socket.emit('message-group', { gid, message, type })
-// 接收到用户的消息
-// socket.on('message', { uid, message, type })
-// 接收到群消息
-// socket.on('message-group', { gid, message, type })
-// 接收到公告消息
-// socket.on('notice', { nid, message, type })
-
-export default socket;
+  return socket;
+};
--- src/views/home/index.vue
+++ src/views/home/index.vue
@@ -16,16 +16,21 @@
 
 <script setup lang="ts">
 import { ref, watch } from 'vue';
+import { Socket } from 'socket.io-client';
+// eslint-disable-next-line
+import { DefaultEventsMap } from '@socket.io/component-emitter';
 import SideBar from '@/views/layout/sidebar.vue';
 import UserList from './user_list.vue';
 import ChatModel from './chat_model.vue';
 import NoticeBoard from './notice_board.vue';
-import socket from '@/utils/socket';
+import socketConnection from '@/utils/socket';
 import request from '@/server';
 import { User, UserList as TUserList } from '@/typings/user';
 import { SocketType } from '@/typings/socket';
 import { useUserStore } from '@/store/user';
 
+// socket对象
+let socket: Socket<DefaultEventsMap, DefaultEventsMap> | null = null;
 // 用户的状态信息
 const userStore = useUserStore();
 
@@ -35,13 +40,16 @@ const chatModelRef = ref<InstanceType<typeof ChatModel> | null>(null);
 
 // 获取用户列表
 const getUserList = async () => {
-  if (!userStore.id) return;
   const { data } = await request.get('/user/list');
   userList.value = data.data;
 };
 
-// 监听用户id的变化获取用户列表
-watch(() => userStore.id, getUserList, {
+// 监听用户id的变化获取用户列表和连接socket
+watch(() => userStore.id, (id) => {
+  if (!id) return;
+  getUserList();
+  socket = socketConnection(id);
+}, {
   immediate: true,
 });
 
@@ -49,7 +57,7 @@ watch(() => userStore.id, getUserList, {
 const send = (message: string, uid: string, type: SocketType = 'text') => {
   console.log(message, uid);
   chatModelRef.value?.clearMessage();
-  socket.emit('message', { uid, message, type });
+  socket?.emit('message', { uid, message, type });
 };
 
 // TODO 用于记录公告信息
